bf68b17ef1974d785218195f9424ae25443b337b
Grzegorz <pokrzywniak.grzegorz@gmail.com>
2018-04-23 22:13:45 +0200

    initial

---
+++ NQueensAndLatinSquareProblem/NQueens/Board.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NQueens
+{
+    public class Board
+    {
+        private bool[] diagonalR;
+        private bool[] diagonalL;
+        private bool[] col;
+        private int size;
+        public int found = 0;
+        public int returns = 0;
+
+        public Board(int size)
+        {
+            diagonalR = new bool[256];
+            diagonalL = new bool[256];
+            col = new bool[size];
+            this.size = size;
+        }
+
+        public void AddQueen_backtracking(int row)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (!((col[i]) || (diagonalR[i - row + 128]) || (diagonalL[row + i + 128])))
+                {
+                    diagonalR[i - row + 128] = true;
+                    diagonalL[row + i + 128] = true;
+                    col[i] = true;
+                    if (row < size-1)
+                    {
+                        AddQueen_backtracking(row + 1);
+                    }
+                    else
+                    {
+                        found++;
+                    }
+                    diagonalR[i - row + 128] = false;
+                    diagonalL[row + i + 128] = false;
+                    col[i] = false;
+                    returns++;
+                }
+            }
+        }
+
+        public void AddQueen_forwardChecking(int row)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (!((col[i]) || (diagonalR[i - row + 128]) || (diagonalL[row + i + 128])))
+                {
+                    diagonalR[i - row + 128] = true;
+                    diagonalL[row + i + 128] = true;
+                    col[i] = true;
+                    if (row == size-1 || ForwardChecking(row + 1))
+                    {
+                        if (row < size - 1)
+                        {
+                            AddQueen_forwardChecking(row + 1);
+                        }
+                        else
+                        {
+                            found++;
+                        }
+                    }
+                    diagonalR[i - row + 128] = false;
+                    diagonalL[row + i + 128] = false;
+                    col[i] = false;
+                    returns++;
+                }
+            }
+        }
+
+        public bool ForwardChecking(int row)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (!(col[i]) && !(diagonalR[i - row + 128]) && !(diagonalL[row + i + 128]))
+                {
+                    if (row < size - 1)
+                    {
+                        return ForwardChecking(row + 1);
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        /*public bool ForwardChecking(int row)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (!(col[i]) && !(diagonalR[i - row + 128]) && !(diagonalL[row + i + 128]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }*/
+    }
+}
---
+++ NQueensAndLatinSquareProblem/NQueens/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board backTrackingBoard, forwardCheckingBoard;
+            for (int i = 1; i < 13; i++)
+            {
+                backTrackingBoard = new Board(i);
+                forwardCheckingBoard = new Board(i);
+                var backTrackingWatch = System.Diagnostics.Stopwatch.StartNew();
+                backTrackingBoard.AddQueen_backtracking(0);
+                
+                var elapsedMsBT = backTrackingWatch.ElapsedTicks;
+                backTrackingWatch.Stop();
+                var ForwardCheckingWatch = System.Diagnostics.Stopwatch.StartNew();
+                forwardCheckingBoard.AddQueen_forwardChecking(0);
+                
+                var elapsedMsFC = ForwardCheckingWatch.ElapsedTicks;
+                ForwardCheckingWatch.Stop();
+                Console.WriteLine(i + " BackTracking found: " + backTrackingBoard.found + " in " + elapsedMsBT + " returns " + backTrackingBoard.returns);
+                Console.WriteLine(i + " ForwardChecking found: " + forwardCheckingBoard.found + " in " + elapsedMsFC + " returns " + forwardCheckingBoard.returns);
+            }
+        }
+    }
+}
